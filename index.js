c9e346dffd00e657778eea29b677d1d504d845cc
Ariel <amonaco@gmail.com>
2018-05-14 19:32:44 +0100

    indentation cleanup

--- index.js
+++ index.js
@@ -9,70 +9,67 @@ var missCount = 0;
 exports.data = cache
 
 exports.put = function(family, key, value, time, timeoutCallback) {
-  if (debug) console.log('caching: ' + family + ' / ' + key + ' = ' + value + ' (@' + time + ')');
+    if (debug) console.log('caching: ' + family + ' / ' + key + ' = ' + value + ' (@' + time + ')');
 
-  // initialize if undefined
-  if (typeof(cache[family]) === "undefined") {
-    if (debug) console.log('initializing family: ' + family);
-    cache[family] = {};
-  }
+    // initialize if undefined
+    if (typeof(cache[family]) === "undefined") {
+        if (debug) console.log('initializing family: ' + family);
+            cache[family] = {};
+    }
 
-  // check if defined...
-  var oldRecord = cache[family][key];
+    // check if defined...
+    var oldRecord = cache[family][key];
+    if (oldRecord) {
+        clearTimeout(oldRecord.timeout);
+    }
 
-	if (oldRecord) {
-		clearTimeout(oldRecord.timeout);
-	}
+    var expire = time + now();
+    var record = {value: value, expire: expire};
 
-	var expire = time + now();
-	var record = {value: value, expire: expire};
-
-	if (!isNaN(expire)) {
-      var timeout = setTimeout(function() {
-	    exports.del(family, key);
-	    if (typeof timeoutCallback === 'function') {
-        if (debug) console.log("adding default timeout callback.")
-	    	timeoutCallback(family, key);
-	    }
-	  }, time);
-    record.timeout = timeout;
-	}
-
-	cache[family][key] = record;
+    if (!isNaN(expire)) {
+        var timeout = setTimeout(function() {
+            exports.del(family, key);
+            if (typeof timeoutCallback === 'function') {
+                if (debug) console.log("adding default timeout callback.")
+                timeoutCallback(family, key);
+            }
+        }, time);
+        record.timeout = timeout;
+    }
+    cache[family][key] = record;
 }
 
 exports.del = function(family, key) {
-
-  if (key) {
-    delete cache[family][key];
-  } else {
-    delete cache[family];
-  }
+    if (key) {
+        delete cache[family][key];
+    } else {
+        delete cache[family];
+    }
 }
 
 exports.clear = function() {
-  cache = {};
+      cache = {};
 }
 
 exports.get = function(family, key) {
 
-  if (typeof(cache[family]) !== "undefined" &&
-      typeof(cache[family][key]) !== "undefined") {
+    if (typeof(cache[family]) !== "undefined" &&
+        typeof(cache[family][key]) !== "undefined") {
 
-    var data = cache[family][key];
+        var data = cache[family][key];
     
-    if (isNaN(data.expire) || data.expire >= now()) {
-	  if (debug) hitCount++;
-      return data.value;
-    } else {
-      // free some space
-      if (debug) missCount++;
-      exports.del(family, key);
+        if (isNaN(data.expire) || data.expire >= now()) {
+            if (debug) hitCount++;
+            return data.value;
+        } else {
+            // free some space
+            if (debug) missCount++;
+            exports.del(family, key);
+        }
+    } else if (debug) {
+        missCount++;
     }
-  } else if (debug) {
-    missCount++;
-  }
-  return null;
+    return null;
 }
 
 exports.expires = function(family, key) {
@@ -82,45 +79,43 @@ exports.expires = function(family, key) {
 
 exports.exists = function(family, key) {
 
-  if (typeof(cache[family]) !== "undefined" &&
-      typeof(cache[family][key]) !== "undefined") {
+    if (typeof(cache[family]) !== "undefined" &&
+        typeof(cache[family][key]) !== "undefined") {
 
-    var data = cache[family][key];
-    if (typeof data != "undefined") {
-      return true;
+        var data = cache[family][key];
+        if (typeof data != "undefined") {
+            return true;
+        }
     }
-  } 
-
-  return false;
+    return false;
 }
 
 exports.size = function(family) { 
-  
-  var pool;
+    var pool;
 
-  if (typeof(cache[family]) != "undefined") {
-    pool = cache[family];
-  } else {
-    pool = cache;
-  }
+    if (typeof(cache[family]) != "undefined") {
+        pool = cache[family];
+    } else {
+        pool = cache;
+    }
 
-  var size = 0, key;
-  for (key in pool) {
-    if (pool.hasOwnProperty(key)) 
-      if (exports.get(family, key) !== null)
-        size++;
-  }
-  return size;
+    var size = 0, key;
+    for (key in pool) {
+        if (pool.hasOwnProperty(key)) 
+            if (exports.get(family, key) !== null)
+              size++;
+    }
+    return size;
 }
 
 exports.debug = function(bool) {
-  debug = bool;
+    debug = bool;
 }
 
 exports.hits = function() {
-	return hitCount;
+    return hitCount;
 }
 
 exports.misses = function() {
-	return missCount;
+    return missCount;
 }
